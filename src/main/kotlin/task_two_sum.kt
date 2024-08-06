2b54cea01a45ed130eafe2d6940fa3ca53c6688d
Alex Lenkov <alex.lenkov123@outlook.com>
2024-08-06 12:17:32 +0300

    corrected solution

--- src/main/kotlin/task_two_sum.kt
+++ src/main/kotlin/task_two_sum.kt
@@ -3,8 +3,22 @@
 
 fun main() {
     val intArray: IntArray = intArrayOf(2,7,11,15)
-    val solution = Solution()
 
-    println(solution.twoSum(intArray, 26).contentToString())
+    println(twoSum(intArray, 26).contentToString())
 
+}
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    for (i in 0 until nums.size) {
+        val firstNum = nums[i]
+
+        for (j in i + 1 until nums.size) {
+            val secondNum = nums[j]
+            if (firstNum + secondNum == target) {
+                val result = intArrayOf(i,j)
+                return result
+            }
+        }
+    }
+    return intArrayOf()
 }
