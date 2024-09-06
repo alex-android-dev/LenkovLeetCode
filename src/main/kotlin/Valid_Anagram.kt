8d0b7a5da73fcebf64b00092f8b796484e721ea5
avlenkov <alex.lenkov@mail.ru>
2024-09-06 10:42:28 +0300

    update solution

--- src/main/kotlin/Valid_Anagram.kt
+++ src/main/kotlin/Valid_Anagram.kt
@@ -1,36 +1,15 @@
 fun main() {
     val solution: Solution = Solution()
 
-    println(solution.isAnagram("aacc", "ccac"))
+    println(solution.isAnagram("var", "rav"))
 }
 
 
 class Solution {
     fun isAnagram(s: String, t: String): Boolean {
-
         if (s.length != t.length) return false
-
-        val firstWordList: MutableList<String> = mutableListOf()
-        val secondWordList: MutableList<String?> = mutableListOf()
-
-        s.map { firstWordList.add(it.toString()) }
-        t.map { secondWordList.add(it.toString()) }
-
-        for (ind in firstWordList.indices) {
-            val str = firstWordList[ind]
-            for (ind in secondWordList.indices) {
-                if (str == secondWordList[ind]) {
-                    secondWordList[ind] = null
-                    break
-                }
-            }
-        }
-
-        return if (secondWordList.all { it == null }) {
-            true
-        } else {
-            false
-        }
-
+        if (s.toCharArray().sorted() ==
+            t.toCharArray().sorted()) return true
+        return false
     }
 }
