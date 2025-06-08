b500cb31cb71308d75e5c5f1ddd1025b1de77fb9
avlnkov <alex.lenkov@mail.ru>
2025-06-08 23:37:08 +0300

    улучшил скорость

--- src/main/kotlin/Valid_Anagram/Valid_Anagram.kt
+++ src/main/kotlin/Valid_Anagram/Valid_Anagram.kt
@@ -1,17 +1,48 @@
 package Valid_Anagram
 
+import java.util.Stack
+
+/**
+ * Принимает, но долгая скорость
+ */
+
 fun main() {
-    val solution: Solution = Solution()
+    val solution = Solution()
 
     println(solution.isAnagram("var", "rav"))
+    println(solution.isAnagram("car", "rac"))
+    println(solution.isAnagram("car", "cer"))
+    println(solution.isAnagram("ab", "a"))
 }
 
 
 class Solution {
     fun isAnagram(s: String, t: String): Boolean {
+
         if (s.length != t.length) return false
-        if (s.toCharArray().sorted() ==
-            t.toCharArray().sorted()) return true
-        return false
+
+        val hashMap = HashMap<Char, Int>()
+
+        /**
+         * Теперь у нас есть мапа с частотностью символов
+         */
+        s.forEach { char ->
+            hashMap.put(char, hashMap.getOrDefault(char, 0) + 1)
+        }
+
+        t.forEach { char ->
+
+            if (!hashMap.contains(char)) {
+                return false
+            } else if (hashMap.get(char) == 1) {
+                hashMap.remove(char)
+            } else {
+                hashMap.put(char, (hashMap.get(char)!! - 1))
+            }
+
+        }
+
+        return true
+
     }
 }
